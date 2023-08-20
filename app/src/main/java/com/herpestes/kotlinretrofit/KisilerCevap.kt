f7e5e682c74eed8ea6a428e512f3684ded109118
herpestes <ahmetbeyazoglu@gmail.com>
2023-08-20 22:07:18 +0300

    JsonResponse

---
+++ app/src/main/java/com/herpestes/kotlinretrofit/KisilerCevap.kt
@@ -0,0 +1,10 @@
+package com.herpestes.kotlinretrofit
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+data class KisilerCevap(
+    @SerializedName("kisiler") @Expose var kisiler: List<Kisiler>,
+    @SerializedName("success") @Expose var success: Int
+) {
+}
