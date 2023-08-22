74bc425cbf8eac878531cc3c9ff6869e81fe6d8e
herpestes <ahmetbeyazoglu@gmail.com>
2023-08-22 20:19:41 +0300

    JsonResponse

--- app/src/main/java/com/herpestes/kotlinretrofit/KisilerDaoInterface.kt
+++ app/src/main/java/com/herpestes/kotlinretrofit/KisilerDaoInterface.kt
@@ -1,5 +1,11 @@
 package com.herpestes.kotlinretrofit
 
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface KisilerDaoInterface {
+
+    @GET("/kisiler/tum_kisiler.php")
+    fun tumKisiler():Call<KisilerCevap>
 
 }
--- app/src/main/java/com/herpestes/kotlinretrofit/MainActivity.kt
+++ app/src/main/java/com/herpestes/kotlinretrofit/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.herpestes.kotlinretrofit
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -8,9 +9,13 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.herpestes.kotlinretrofit.ui.theme.KotlinRetrofitTheme
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,8 +36,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Sayfa() {
+    LaunchedEffect(key1 = true){
 
+    }
 }
+fun tumKisiler(){
+    val kisilerDaoInterface = ApiUtils.getKisilerGetInterface()
+
+    kisilerDaoInterface.tumKisiler().enqueue(object: Callback<KisilerCevap>{
+        override fun onResponse(call: Call<KisilerCevap>, response: Response<KisilerCevap>) {
+            val liste = response.body().kisiler
+            for(k in liste){
+                Log.e("Kisi bilgi", "**************")
+                Log.e("Kisi id: ", k.kisi_id.toString())
+                Log.e("Kisi ad: ", k.kisi_ad)
+                Log.e("Kisi tel: ", k.kisi_tel)
+            }
+        }
+
+        override fun onFailure(call: Call<KisilerCevap>?, t: Throwable?) {}
+    })
+}
+
 
 @Preview(showBackground = true)
 @Composable
